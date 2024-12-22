f8531459f1968db9d5ba9fbe1c894e8271433767
Stephen <medicup@gmail.com>
2024-12-22 17:09:54 -0500

    Minor refactor and clean up

---
+++ config_files/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py for config_files
+
+"""
+This package contains configuration files for the auto_updater_package.
+"""
+
+# Optionally, you can expose constants or utility functions here if needed.
+# For example:
+# CONFIG_FILES_PATH = os.path.dirname(__file__)
