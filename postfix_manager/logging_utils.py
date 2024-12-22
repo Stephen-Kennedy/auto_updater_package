1106f543efab1da1831e2f363aaedee9bd2d6076
Stephen <github@red6.slmail.me>
2024-12-21 21:31:13 -0500

    Update logging_utils.py

--- postfix_manager/logging_utils.py
+++ postfix_manager/logging_utils.py
@@ -1,1 +1,13 @@
-# logging_utils module
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+def setup_logger(name, log_file, level=logging.INFO, max_bytes=5 * 1024 * 1024, backup_count=3):
+    """Set up a logger with rotation."""
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
