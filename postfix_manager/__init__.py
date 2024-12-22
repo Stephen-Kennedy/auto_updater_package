f42adb302a81e83c53f0ee4b1801783dea980f24
root <root@redmail.stephenjkennedy.me>
2024-12-22 19:41:46 +0000

    corrected error in __init__.py

--- postfix_manager/__init__.py
+++ postfix_manager/__init__.py
@@ -2,4 +2,4 @@
 from postfix_manager.auto_update import main as auto_update_main
 from postfix_manager.postfix_setup import main as postfix_setup_main
 from postfix_manager.update_pihole import main as update_pihole_main
-from postfix_manager.
+
