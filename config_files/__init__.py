c9dfdce5bcca5dad0b20caa05a3fa3aca2f7a902
root <root@redmail.stephenjkennedy.me>
2024-12-22 22:28:32 +0000

    minor revisions to config_vim

--- config_files/__init__.py
+++ config_files/__init__.py
@@ -1,9 +1,8 @@
 # __init__.py for config_files
 
-"""
-This package contains configuration files for the auto_updater_package.
-"""
+auto_updater_package/
+    __init__.py
+    config_files/
+        __init__.py
+        vim_utils.txt
 
-# Optionally, you can expose constants or utility functions here if needed.
-# For example:
-# CONFIG_FILES_PATH = os.path.dirname(__file__)
